0e23b56135b3be8e197887aa7045f579e261082e
7Davronbek <maxmudovdavronbek8@gmail.com>
2024-01-17 19:06:29 +0500

    inite

---
+++ src/page/main/Main.tsx
@@ -0,0 +1,16 @@
+const Main = () => {
+        return (
+            <div className="Main">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            MAIN
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+;
+
+export default Main;
